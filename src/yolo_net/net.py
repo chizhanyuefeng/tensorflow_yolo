e0955e5ab23fcce66315d5f94bf6ff5fa548ecb9
chizhanyuefeng <chizhanyuefeng@gmail.com>
2018-04-27 12:52:21 +0800

    修改配置文件，并继续实现net基类

--- src/yolo_net/net.py
+++ src/yolo_net/net.py
@@ -1,16 +1,47 @@
+import tensorflow as tf
+
 
 class Net(object):
     '''
     Base Net class
     '''
+
+    _batch_size = 0
+    _image_size = 0
+    _momentum = 0
+    _decay = 0
+    _learning_rate = 0
+    _max_objects = 0
+
     def __init__(self):
         '''
         构建网络
         '''
         pass
 
-    def _conv2d_layer(self,name):
-        pass
+    def _conv2d_layer(self,name,input,kernel_size,output_size,stride,):
+        with tf.name_scope(name):
+            input_size = tf.shape(input)[3]
+
+            wights = self._variable_wights(kernel_size,input_size,output_size)
+            biases = self._variable_biases(output_size)
+            conv = tf.nn.conv2d(input,wights,strides=[1,stride,stride,1],padding='SAME')
+            layer_output = tf.add(conv,biases)
+
+            return self._activation_func(layer_output)
+
+    def _activation_func(self,x):
+        '''
+        激活函数，见yolo_v1论文
+        if x >0:
+            x=x
+        else
+            x= alpha*x
+        '''
+        temp_x = tf.cast(x>0,tf.float32)
+
+        return tf.add(tf.multiply(temp_x,x),tf.multiply(1-temp_x,x)*self._alpha)
+
 
     def _max_pooling_layer(self,name):
         pass
@@ -18,7 +49,7 @@ class Net(object):
     def _connection_layer(self,name):
         pass
 
-    def _variable_wights(self,shape):
+    def _variable_wights(self,kernel_size,inpout_size,output_size):
         pass
 
     def _variable_biases(self,shape):
